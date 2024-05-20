bbb7e7f69f34b6ed1f5dc8e8e96323523ce262d0
tsholofelomoetse <fcse23-009@thuto.bac.ac.bw>
2024-05-20 22:18:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+  var menu = document.querySelector('.menu');
+  menu.classList.toggle('show');
+} 
+    function openTrailer() {
+      document.getElementById('trailerVideo').style.display = 'block';
+    }
+  
+
+  
